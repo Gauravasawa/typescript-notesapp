2daa0d387834345f619ff865f2d6f012fd998774
gaurav asawa <gaurav.asawa@asychron.com>
2022-06-16 17:10:35 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
 import './App.css';
 
 function App() {
   const a = 10;
 
+  const b = 5;
+
   return (
     <div>
       <h1>{a}</h1>
+      <h1>{b}</h1>
     </div>
   );
 }
