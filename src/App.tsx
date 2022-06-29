2a92ef72b8fd03a99c477ccffd318d77958b0eb0
gaurav asawa <gaurav.asawa@asychron.com>
2022-06-29 17:53:33 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
-import { useState } from 'react';
-import './App.css';
-import Header from './components/Header';
-import { Note } from './components/models/note.model';
-
 function App() {
-  const [notes, setNotes] = useState<Note[]>([
-    {
-      id: new Date().toString(),
-      title: 'Meetings',
-      text: 'Schedule Meeting',
-      color: '#dfdfdf',
-      date: new Date().toString(),
-    },
-  ]);
+ 
+  const a = 30;
 
-  return <Header />;
+  return (
+    <>
+     {a}
+    </>
+  );
 }
 
 export default App;
--- src/components/Createnotes.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Createnotes() {
-  return <div>Createnotes</div>;
-}
-
-export default Createnotes;
